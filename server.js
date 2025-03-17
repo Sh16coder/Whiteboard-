658154cfa7ac89e5c26ec93f857d722dfa7a10c7
Sh16coder <158461477+Sh16coder@users.noreply.github.com>
2025-03-17 17:05:42 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,1 +1,34 @@
+const express = require("express");
+const http = require("http");
+const { Server } = require("socket.io");
+const cors = require("cors");
 
+const app = express();
+const server = http.createServer(app);
+const io = new Server(server, {
+    cors: { origin: "*" } // Allow all origins
+});
+
+app.use(cors());
+app.use(express.static(__dirname + "/public"));
+
+io.on("connection", (socket) => {
+    console.log("User connected");
+
+    socket.on("draw", (data) => {
+        socket.broadcast.emit("draw", data);
+    });
+
+    socket.on("clear", () => {
+        io.emit("clear");
+    });
+
+    socket.on("disconnect", () => {
+        console.log("User disconnected");
+    });
+});
+
+const PORT = process.env.PORT || 3000;
+server.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+});
